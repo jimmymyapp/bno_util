929a4ba352f404a44b12f5b565371db58a6ed0e4
Jimmy Li <81349106+aenckli@users.noreply.github.com>
2025-04-14 12:04:52 +0100

    added tooltip chinese translation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"010c8a806bccad64ab9972286b85dd24ca9844
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2758052459"
+    serviceWorkerVersion: "3411668980"
   }
 });
