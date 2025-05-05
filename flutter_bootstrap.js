87eb13cfbdac86c8054a6f0c822d82a49caf54f4
Jimmy Li <81349106+aenckli@users.noreply.github.com>
2025-05-05 09:47:50 +0100

    forget to switch to web from android in last build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "118813638"
+    serviceWorkerVersion: "3604040840"
   }
 });
